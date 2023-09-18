f873de4ab8afd56f051b0633248c309266328068
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-09-18 14:30:00 +0200

    UE00: Components Methods

--- src/UE00_WHP/Components.java
+++ src/UE00_WHP/Components.java
@@ -5,4 +5,44 @@ import java.util.Map;
 
 public abstract class Components {
 
+
+    boolean[] interfaces;
+
+    /**
+     * Inputs and Outputs
+     */
+    protected final Map<String, Cable> values = new HashMap<String, Cable>();
+
+    /**
+     * Constructor
+     * @param interfaces is the amount of interfaces
+     */
+    public Components(int interfaces) {
+        this.interfaces = new boolean[interfaces];
+    }
+
+    public abstract void pull();
+
+    /**
+     * Inputs verarbeiten und Ausgänge setzen
+     */
+    public abstract void calc();
+
+    /**
+     * Getter für Input Zustände
+     * @param input um den index anzugeben vom input
+     * @return bool
+     */
+    public boolean getInputs(int input) {
+        return interfaces[input];
+    }
+
+    /**
+     * toString method
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "{" + values + '}';
+    }
 }
