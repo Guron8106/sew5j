fb7f6cb5bbec396b9ed5162638337a4c066b7342
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-09-18 15:58:00 +0200

    UE00: Cable init, methods and reformation

---
+++ src/UE00_WHP/Cable.java
@@ -0,0 +1,40 @@
+package UE00_WHP;
+
+/**
+ * Cable
+ * @author Karanbir Guron
+ */
+
+public class Cable {
+
+    /**
+     * Überprüft ein Signal
+     */
+    private boolean isOn = false;
+
+    /**
+     * Getter für isON
+     * @return isOn
+     */
+    public boolean isOn() {
+        return isOn;
+    }
+
+    /**
+     * Setter für isOn
+     * @param on turning it on
+     */
+    public void setOn(boolean on) {
+        isOn = on;
+    }
+
+    /**
+     * toString
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "{" + "isOn=" + isOn + '}';
+    }
+
+}
