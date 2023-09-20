a39fd9778e14ea46b110f2e9039d55ad6494b224
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-09-20 18:47:00 +0200

    UE00: AND constructor and variables

--- src/UE00_WHP/AND.java
+++ src/UE00_WHP/AND.java
@@ -6,5 +6,30 @@ package UE00_WHP;
  */
 public class AND extends Components {
 
+    /**
+     * Gepullter Zustand A
+     */
+    private static final int IA = 0;
+
+    /**
+     * Gepullter Zustand B
+     */
+    private static final int IB = 1;
+
+
+    /**
+     * Konstruktor
+     *
+     * @param a Connection Eingang A
+     * @param b Connection Eingang B
+     * @param x Connection Ausgang X
+     */
+    public AND(Cable a, Cable b, Cable x) {
+        super(2);
+        values.put("A", a);
+        values.put("B", b);
+        values.put("X", x);
+    }
+
 
 }
