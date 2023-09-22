aebab0573d692a3f50607701fbb6958917592ebe
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-09-22 22:34:00 +0200

    UE00: LED pull and calc

--- src/UE00_WHP/LED.java
+++ src/UE00_WHP/LED.java
@@ -20,5 +20,24 @@ public class LED extends Components {
         values.put("I", connection);
     }
 
+    /**
+     * Alle Inputs pullen
+     */
+    @Override
+    public void pull() {
+        interfaces[I1] = values.get("I").isOn();
+    }
+
+    /**
+     * Inputs verarbeiten und Ausgänge setzen
+     */
+    @Override
+    public void calc() {
+        if (interfaces[I1]) {
+            System.out.println("LED: ON");
+        } else {
+            System.out.println("LED: OFF");
+        }
+    }
 
 }
