20105fe44a43dd9df4a7120209be6726c02f2f00
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-09-24 09:58:00 +0200

    UE00: runCircuit constructor

--- src/UE00_WHP/runCircuit.java
+++ src/UE00_WHP/runCircuit.java
@@ -6,6 +6,17 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class runCircuit {
+    protected final List<Components> components = new LinkedList<>();
+
+    /**
+     * Konstruktor
+     * @param components Komponenten
+     */
+    public runCircuit(Components... components) {
+        this.components.addAll(Arrays.asList(components));
+    }
+
+
 
 
 }
