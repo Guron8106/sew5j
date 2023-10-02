b81539c7035aceeeca943a80b17d3a18fadfea6e
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-10-02 15:07:00 +0200

    UE00: Abnahme durch WAG

--- src/UE00_WHP/Main.java
+++ src/UE00_WHP/Main.java
@@ -2,16 +2,19 @@ package UE00_WHP;
 
 public class Main {
     public static void main(String[] args) {
+        // Cable
         Cable t1_ff = new Cable();
         Cable t2_ff = new Cable();
         Cable ff_led = new Cable();
         Cable ff_led2 = new Cable();
 
+        //Taster
         Taster t1 = new Taster(t1_ff);
         Taster t2 = new Taster(t2_ff);
         FlipFlop ff = new FlipFlop(t1_ff, t2_ff, ff_led, ff_led2);
         LED led = new LED(ff_led);
         LED led2 = new LED(ff_led);
+
 
         runCircuit tact = new runCircuit(t1, t2, ff, led, led2);
 
