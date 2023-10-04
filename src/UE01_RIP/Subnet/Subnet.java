c09bcea1685b6f567261c4799dda496766e7e161
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-10-04 20:04:00 +0200

    UE01: Subnet adding Constructor and createMask

--- src/UE01_RIP/Subnet/Subnet.java
+++ src/UE01_RIP/Subnet/Subnet.java
@@ -2,4 +2,28 @@ package UE01_RIP.Subnet;
 
 public class Subnet implements Comparable {
 
+    /**
+     * network address
+     */
+    private IPAddress net;
+    /**
+     * network mask
+     */
+    private IPAddress mask;
+
+    /**
+     * create netmask from network ip and number of bits
+     * @param net     network address
+     * @param cidr    number of bits
+     */
+    public Subnet(IPAddress net, int cidr) {
+        createMask(net, cidr);
+    }
+
+
+
+    private void createMask(IPAddress net, int cidr) {
+        this.net = net;
+
+    }
 }
