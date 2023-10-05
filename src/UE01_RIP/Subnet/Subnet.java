5ed09e2bd25d1bf581d295f107a1252fea9d2556
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-10-05 16:14:00 +0200

    UE01: IPAddress adding Constructor

--- src/UE01_RIP/Subnet/Subnet.java
+++ src/UE01_RIP/Subnet/Subnet.java
@@ -20,10 +20,36 @@ public class Subnet implements Comparable {
         createMask(net, cidr);
     }
 
-
-
     private void createMask(IPAddress net, int cidr) {
         this.net = net;
+        this.mask = IPAddress.createNetmask(cidr);
+
+        if ((this.net.getIP() & mask.getIP()) != this.net.getIP() || cidr>32 || cidr<0) {
+            throw new IllegalArgumentException("bad network");
+        }
 
     }
+
+    /**
+     * create netmask from ip (four number) and number of bits
+     * @param a3
+     * @param a2
+     * @param a1
+     * @param a0
+     * @param cidr
+     */
+    public Subnet(int a3, int a2, int a1, int a0, int cidr) {
+        this(new IPAddress(a3, a2, a1, a0), cidr);
+    }
+
+    public Subnet(String mask) {
+        String[] parts = mask.split("/");
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("ill formed subnet");
+        }
+        IPAddress ip = new IPAddress(parts[0]);
+        int cidr = Integer.parseInt(parts[1]);
+        createMask(ip, cidr);
+    }
+
 }
