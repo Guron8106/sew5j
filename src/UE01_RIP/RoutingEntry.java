52d2171e1fd5569d217aaaf0cdcef2d250746cf6
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-10-07 14:31:00 +0200

    UE01: RoutingEntry

---
+++ src/UE01_RIP/RoutingEntry.java
@@ -0,0 +1,31 @@
+package UE01_RIP;
+
+public class RoutingEntry {
+
+
+    private int hopCount;
+
+    private final Router neighbour;
+
+    public RoutingEntry(int hopCount, Router neighbour){
+        this.hopCount = hopCount;
+        this.neighbour = neighbour;
+    }
+
+    public Router getNeighbour(){
+        return neighbour;
+    }
+
+    public int getHopCount(){
+        return hopCount;
+    }
+
+    @Override
+    public String toString(){
+       return  "RoutingEntry [Hopcount: " + hopCount + ", Neighbour: " + (neighbour!=null ? neighbour.toString() : "Myself") ;
+    }
+    public void setHopCount(int hopCount){
+        this.hopCount = hopCount;
+    }
+
+}
