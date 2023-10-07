974885ce983d5687ed1b03b95cae57541b2306a8
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-10-07 13:21:00 +0200

    UE01: Route-Packet finished

---
+++ src/UE01_RIP/Packets/Route.java
@@ -0,0 +1,67 @@
+package UE01_RIP.Packets;
+
+import UE01_RIP.RipObjects.Subnet.Subnet;
+
+public class Route {
+
+    /**
+     * Subnet der Route
+     */
+    public Subnet subnet;
+
+    /**
+     * hopCount der Route
+     */
+    public int hopCount;
+
+    /**
+     * Konstruktor der Klasse DistributedRoute
+     * @param subnet Subnet
+     * @param hopCount hopCount
+     */
+    public Route(Subnet subnet, int hopCount){
+        this.subnet = subnet;
+        this.hopCount = hopCount;
+    }
+
+    /**
+     * Getter-Methode für 'hopCount'
+     * @return 'hopCount'
+     */
+    public int getHopCount() {
+        return hopCount;
+    }
+
+    /**
+     * Getter-Methode für 'subnet'
+     * @return 'subnet'
+     */
+    public Subnet getSubnet() {
+        return subnet;
+    }
+
+    /**
+     * Equals Methode der DistributedRoute Klasse
+     * @param o anderes DistributedRoute Objekt
+     * @return true/false
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Route route = (Route) o;
+
+        if (hopCount != route.hopCount) return false;
+        return subnet.equals(route.subnet);
+    }
+
+    /**
+     * HashCode Methode der DistributedRoute Klasse
+     * @return hashCode
+     */
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
